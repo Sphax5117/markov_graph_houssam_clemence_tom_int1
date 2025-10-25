ab7a9c4fced27489b808acfe69a976825eb4eb96
Sphax5117 <79364242+Sphax5117@users.noreply.github.com>
2025-10-25 15:03:36 +0200

    finished step2

---
+++ markov.c
@@ -0,0 +1,21 @@
+#include "markov.h"
+
+int isMarkovGraph(t_adjacency_list * adj) {
+    int result = 0;
+    for (int i = 0; i < adj->size; i++) {
+        const t_cell *curr = adj->array[i].head;
+        float totalproba = 0;
+        while (curr) {
+            totalproba+= curr->probability;
+            curr = curr->next; 
+        }
+        if (totalproba <= 1 && totalproba >= 0.99) {
+            printf("The sum of probabilities for vertex %d is %.2f\n", i + 1, totalproba);
+            result += 1;
+        } else {
+            printf("The sum of probabilities for vertex %d is %.2f, which is not between 0.99 and 1 \n", i + 1, totalproba);
+            return 0;
+        }
+    }
+    return (result == adj->size);
+}
---
+++ markov.h
@@ -0,0 +1,6 @@
+#ifndef __MARKOV_H__
+#define __MARKOV_H__
+#include "adjacency.h"
+
+int isMarkovGraph(t_adjacency_list * adjList);
+#endif
